5d3c45d015cfeb5020fa16f16e527aa73a1c72ee
kundanln <kundannarkhede93@gmail.com>
2018-11-30 00:21:20 -0800

    changes default chnags

--- src/app/server.js
+++ src/app/server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/CMS-v02'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/CMS-v02/index.html'));
+res.sendFile(path.join(__dirname,'./dist/CMS-v02/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
