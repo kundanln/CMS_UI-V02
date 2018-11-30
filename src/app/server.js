5dcc6ca73c2e4fbeba8498bca28d85b7928d902e
kundanln <kundannarkhede93@gmail.com>
2018-11-29 22:59:02 -0800

    server.js modified

--- src/app/server.js
+++ src/app/server.js
@@ -5,12 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/cms-v02'));
+app.use(express.static('./dist/CMS-v02'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/cms-v02/index.html'));
+res.sendFile(path.join(__dirname,'/dist/CMS-v02/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, ()=>{
+	console.log('server started');
+})
